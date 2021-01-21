e4825d6e333c8d441340c023e9531e2e8ac22d3d
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-01-22 01:36:03 +0530

    Update statistics.py
    updated the method

--- statistics.py
+++ statistics.py
@@ -1,3 +1,19 @@
 
 def calculateStats(numbers):
-  return None
+  
+  #import math
+
+def calculateStats(number):
+    n = len(number)
+    if n < 1:
+        calculateStats.a = None
+        #Sort=number.sort()
+        calculateStats.b = None
+        calculateStats.c = None
+    else :
+        calculateStats.a = float(sum(number)/len(number))
+        #Sort=number.sort()
+        calculateStats.b = min(number)
+        calculateStats.c = max(number)
+ 
+    return {"avg": calculateStats.a, "min": calculateStats.b , "max": calculateStats.c}
