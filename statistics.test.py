7e7e4de60438ff23886aa6274dfcd47962a1d47f
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-01-23 00:10:09 +0530

    Update statistics.test.py
    Updated

--- statistics.test.py
+++ statistics.test.py
@@ -1,5 +1,6 @@
 import unittest
 import statistics
+import math
 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
@@ -14,8 +15,9 @@ class StatsTest(unittest.TestCase):
     # All fields of computedStats (average, max, min) must be
     # nan (not-a-number), as defined in the math package
     # Design the assert here.
-    assert n != 0, "Empty List"
-    return None
+    self.assertIs(computedStats["avg"], math.nan, 'mean requires at least one data point')
+    self.assertIs(computedStats["max"], math.nan)
+    self.assertIs(computedStats["min"], math.nan)
     # Use nan and isnan in https://docs.python.org/3/library/math.html
 
   def test_raise_alerts_when_max_above_threshold(self):
